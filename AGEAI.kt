8e41440d35bc61619f99c75743dca8575d93525c
Daniel J Flynn <53352344+danmanfan@users.noreply.github.com>
2025-09-01 00:56:27 -0700

    Create AGEAI.kt

---
+++ AGEAI.kt
@@ -0,0 +1,7 @@
+/**
+Daniel Flynn
+Autumn 2025
+
+Abstract class that should encompass all aspects of AI and its subsets. AI should include the handling of game logic as well as actors' actions. 
+
+*/
