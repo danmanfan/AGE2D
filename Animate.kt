facea2dbbfba69b42309808b31dc3b8c1ced31b6
Daniel J Flynn <53352344+danmanfan@users.noreply.github.com>
2025-09-07 12:01:50 -0700

    Update Animate.kt
    Added variables for max health cap, max health and health

--- Animate.kt
+++ Animate.kt
@@ -5,5 +5,45 @@ Autumn 2025
 
 class Animate : Actor() {
 
+  /**
+  *  The maximum health cap for an animate object to achieve
+  *
+  */
+  open var MAX_HEALTH_CAP : Int = 999
+    get() = field
+    set(value) {
+      
+    }
+
+  /**
+  * The current maximum health that an animate object has. Cannot exceed MAX_HEALTH_CAP.
+  *
+  */
+  open var maxHealth : Int = 10
+    get() = field
+    set(value) {
+      if(value <= MAX_HEALTH_CAP) {
+        field f = value
+      }
+    }
+
+  /**
+  * The current health that an animate object has. Cannot exceed maxHealth
+  *
+  */
+  open var health : Int = 10
+    get() = field
+    set(value) {
+      if(value <= maxHealth) {
+        field = value
+      } else if (value <= 0) {
+        health = 0
+      }
+    }
+  
+
+  
 
 }
+
+
