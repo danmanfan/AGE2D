8d864d85cc1158a2a900e6c384a256992494a772
Daniel J Flynn <53352344+danmanfan@users.noreply.github.com>
2025-09-22 17:23:32 -0700

    Update AGEPhysics.kt
    Added the quadratic equation to solve for time. Time is to be used as frames and not actually time for this game engine to work.

--- AGEPhysics.kt
+++ AGEPhysics.kt
@@ -2,3 +2,32 @@
 Daniel Flynn
 Autumn 2025
 */
+
+import kotlin.math.*
+
+class AGEPhysics() {
+
+
+  fun quadratic_t(a : Float, b : Float, c : Float) {
+    var x = b.pow(2) - (4*a*c)
+    var t = 0
+    if(x < 0){
+      return -1
+    } 
+    t = (-b + sqrt(x)) / (2*a)
+    return t
+
+  }
+
+  fun solveForT(acc : Float, veloc : Float, posF : Float, posI : Float) {
+    var a = acc / 2
+    var b = veloc
+    var c = posI - posF
+    var t = quadratic_t(a,b,c)
+    return t
+    
+
+  }
+
+
+}
